94d740a1e402a32d527762b41c31e7f7d5eb1a3d
thektm <138784494+thektm@users.noreply.github.com>
2025-12-05 20:07:13 +0330

    initial

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from "next";
+import bundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -6,4 +11,4 @@ const nextConfig: NextConfig = {
   devIndicators: false,
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
